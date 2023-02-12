557b01bef5f5cfee549566e52e9a699897b303a6
walter-noack <wnoack@gmail.com>
2023-02-11 22:30:18 -0300

    ajuste conexion bbdd

--- database/config.js
+++ database/config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+
 const stringConnection = async() =>{
     try {
-        await mongoose.connect (process.env.DB_CNN);
+        await mongoose.connect ('mongodb+srv://wnoack:Wnoack1987@cluster0.b9utksm.mongodb.net/test');
         console.log('tai conectao a la BBDD');
     } catch(error) {
         console.log(error);
